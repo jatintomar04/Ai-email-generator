483011349858dc430849b685ee366366398b0659
jatintomar04 <jatin.tomar.04@gmail.com>
2026-05-09 13:58:13 +0530

    change

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,12 +5,17 @@ const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
         
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+      host: 'smtp.gmail.com',
+  port: 465, // 587 ki jagah 465 try karein (Secure Connection)
+  secure: true, 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false // Connectivity issues bypass karne ke liye
+  }
+});
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
