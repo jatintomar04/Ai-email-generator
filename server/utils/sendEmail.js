a66b50dece7a8f93dc378072282fdd48f5349685
jatintomar04 <jatin.tomar.04@gmail.com>
2026-05-09 13:03:00 +0530

    email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,34 +1,46 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config()
+require("dotenv").config();
 
 const sendEmail = async (to, subject, text) => {
 
-   console.log("EMAIL:", process.env.EMAIL_USER);
+  console.log("EMAIL:", process.env.EMAIL_USER);
   console.log("PASSWORD EXISTS:", !!process.env.EMAIL_PASS);
+
   try {
+
     const transporter = nodemailer.createTransport({
-        
-      service: "gmail",
+
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+      },
+
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
-      to : to,
-      subject : subject,
-      text :  text,
-      html : `<p>${text}</p>`
+      to: to,
+      subject: subject,
+      text: text,
+      html: `<p>${text}</p>`,
     };
 
-    await transporter.sendMail(mailOptions);
-    
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("MAIL SENT:", info.response);
 
   } catch (error) {
+
     console.log("Email error:", error.message);
-     throw new Error(error.message);
+
+    throw new Error(error.message);
   }
 };
 
