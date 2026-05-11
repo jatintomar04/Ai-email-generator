a855d2b9ca28a49a719347db8ca09df7970ef992
jatintomar04 <jatin.tomar.04@gmail.com>
2026-05-11 11:36:57 +0530

    new

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -7,7 +7,9 @@ const sendEmail = async (to, subject, text) => {
       host: "smtp.gmail.com",
       port: 587,
       secure: false,
-      
+      requireTLS :true,
+      logger: true,
+      debug:true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
